b0b2c3a953b109ffe913f0728dae069f3c8cc66b
Typan <Typan@10.181.132.81>
2023-03-24 02:55:23 -0400

    First commit to the project repository

---
+++ src/main/java/Controller/UserController.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author Typan
+ */
+import Model.DAO;
+import Model.User;
+
+
+public class UserController {
+     private DAO theDAO;
+    
+    public UserController() {
+        this.theDAO = new DAO();
+    }
+    
+    public boolean createUser(User theUser) {
+        String encryptedPassword = theDAO.encryptPass(new String(theUser.getPassword()));
+        theUser.setPassword(encryptedPassword.toCharArray());
+        return theDAO.createUser(theUser);
+    }
+    
+    public User getUser(User theUser) {
+        String encryptedPassword = theDAO.encryptPass(new String(theUser.getPassword()));
+        theUser.setPassword(encryptedPassword.toCharArray());
+        return this.theDAO.getUser(theUser);
+    }
+    
+    public void setUserCurrent(String username,String currentAnime){
+        theDAO.setUserCurrent(username, currentAnime);
+    
+    }
+    
+    public String getUserCurrent(String username){
+    
+    return this.theDAO.getUserCurrent(username);
+    }
+    
+    public void setUserFavAnime(String username, String anime1, String anime2, String anime3){
+     
+        this.theDAO.setUserFavAnime(username, anime1, anime2, anime3);
+        
+    }
+     
+    public String getuserFavAnime(String username, String anime){
+    
+     return this.theDAO.getuserFavAnime(username, anime);
+    }
+    
+}
